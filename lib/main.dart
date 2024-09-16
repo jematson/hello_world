155d4c6a2e4ad03dc6d79619fb57adb265a92af1
Jenae Matson <jlmatson@alaska.edu>
2024-09-16 13:29:54 -0800

    feat: add asset images to toggle between

--- lib/main.dart
+++ lib/main.dart
@@ -95,9 +95,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            _counter % 2 == 0 
+              ? const Image(image: AssetImage('assets/satellite.png')) 
+              : const Image(image: AssetImage('assets/waterfall.png')),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
