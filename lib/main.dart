99a380d7608fe5f7e919a36a61ff1941260ef843
Jenae Matson <jlmatson@alaska.edu>
2024-10-01 11:20:30 -0800

    feat: load xml files from url and store locally, implement BookPage class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:xml/xml.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:collection/collection.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,15 +18,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Kulusiinkut'),
@@ -50,49 +43,67 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class BookPage {
+  String image;
+  String text;
+
+  BookPage(this.image, this.text);
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _page = 0;
   List _pageTexts = [];
+  String url = "https://akuzipik.info/books/Kulusiinkut/book.xml";
 
   @override
   void initState() {
     super.initState();
+    fetchAsset();
     loadAsset();
+  }
+
+  // Get path to Documents directory
+  Future<String> get _localPath async {
+    final directory = await getApplicationCacheDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    print(path);
+    return File('$path/book.xml');
+  }
+
+  // Fetch xml from url and write to local file
+  Future<File> fetchAsset() async {
+    final file = await _localFile;
+
+    Map<String, String> headers = {"Accept": "text/html,application/xml"};
+    final response = await http.get(Uri.parse(url), headers: headers);
+    final document = XmlDocument.parse(response.body);
+    return file.writeAsString('$document');
   }
   
   Future<void> loadAsset() async {
-    final pageList = [];
-    String fileText = await rootBundle.loadString('assets/Kulusiinkut/Kulusiinkut/book.xml');
+    List<BookPage> pageList = [];
+    final file = await _localFile;
+    String fileText = await file.readAsString();//rootBundle.loadString('assets/Kulusiinkut/Kulusiinkut/book.xml');
     final document = XmlDocument.parse(fileText);
-    final bookBody = document.findElements('TEI').first.findElements('text').first.findElements('body').first;
-    final pages = bookBody.findAllElements('div');
+    final pages = document.findAllElements('div').where((element) => element.getAttribute('type') == 'page');
 
     for (var page in pages) {
       final pageText = page.innerText;
+      final pageImage = page.findElements('p').first.findElements('graphic').first.getAttribute('url');
 
-      final pageP = page.findElements('p').firstOrNull;
-      String? pageImage;
-      if(pageP != null) {
-        final pageGraphic = pageP.findElements('graphic').firstOrNull;
-        if(pageGraphic != null){
-          pageImage = pageGraphic.getAttribute('url');
-        }
-      }
-      pageList.add({'image': pageImage ?? 'no image available', 'text': pageText});
+      pageList.add(BookPage(pageImage ?? 'no image available', pageText));
     }
     setState(() {
       _pageTexts = pageList;
     });
   }
   
-
   void _incrementPage() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _page without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       if (_page < 22) {
         _page++;
       }
@@ -135,32 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SingleChildScrollView(
-        /*
         child: Column(
-          children: _fileContents.entries.map((entry) {
-            return Text(
-              'Page ${entry.key}: ${entry.value}',
-              style: Theme.of(context).textTheme.bodyLarge,
-            );
-          }).toList(),
-        ),*/
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ConstrainedBox(
@@ -171,11 +157,11 @@ class _MyHomePageState extends State<MyHomePage> {
               child: 
                 _page == 0
                   ? const Image(image: AssetImage('assets/Kulusiinkut/Kulusiinkut/img/100/preview.webp'))
-                  : Image(image: AssetImage('assets/Kulusiinkut/Kulusiinkut/img/100/${_pageTexts[_page]['image']}')) 
-                  //p${_page.toString().padLeft(4, '0')}.webp
+                  : Image(image: AssetImage('assets/Kulusiinkut/Kulusiinkut/img/100/${_pageTexts[_page-1].image}')) 
             ),
             Text(
-              '$_page. ${_pageTexts[_page]['text']}',
+              _page == 0 ? 'Kulusiinkut'
+              :'$_page. ${_pageTexts[_page-1].text}',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             Padding(
